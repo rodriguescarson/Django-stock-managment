21c054e287882a777aad0fc117d6148c7c9cb995
Carson <rodriguescarson@hotmail.com>
2021-11-29 11:55:26 +0530

    edit:alot of things

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -75,13 +75,18 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'your-db-name',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- stockmgmt/admin.py
+++ stockmgmt/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Stock
-from .forms import StockCreateForm
+from .models import Category, Stock
+from .forms import *
+
 
 class StockCreateAdmin(admin.ModelAdmin):
-   list_display = ['category', 'item_name', 'quantity']
-   form = StockCreateForm
-   list_filter = ['category']
-   search_fields = ['category', 'item_name']
+    list_display = ['category', 'item_name', 'quantity']
+    form = StockCreateForm
+    list_filter = ['category']
+    search_fields = ['category', 'item_name']
 
-admin.site.register(Stock,StockCreateAdmin)
+
+admin.site.register(Stock, StockCreateAdmin)
+admin.site.register(Category)
